180746de522473dcf13a0eb96b85530e63f74443
shijinxin <793603478@qq.com>
2018-01-30 19:55:51 +0800

    作业2

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblOp = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.lstDisp = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(27, 46);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(0, 12);
+            this.lblA.TabIndex = 0;
+            // 
+            // lblOp
+            // 
+            this.lblOp.AutoSize = true;
+            this.lblOp.Location = new System.Drawing.Point(85, 46);
+            this.lblOp.Name = "lblOp";
+            this.lblOp.Size = new System.Drawing.Size(0, 12);
+            this.lblOp.TabIndex = 1;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(143, 46);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(0, 12);
+            this.lblB.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "=";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(289, 42);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(114, 21);
+            this.txtAnswer.TabIndex = 4;
+            this.txtAnswer.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(54, 106);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(85, 32);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "出题";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click_1);
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(267, 103);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(80, 35);
+            this.btnJudge.TabIndex = 6;
+            this.btnJudge.Text = "规则";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.btnJudge_Click_1);
+            // 
+            // lstDisp
+            // 
+            this.lstDisp.FormattingEnabled = true;
+            this.lstDisp.ItemHeight = 12;
+            this.lstDisp.Location = new System.Drawing.Point(26, 172);
+            this.lstDisp.Name = "lstDisp";
+            this.lstDisp.Size = new System.Drawing.Size(389, 136);
+            this.lstDisp.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 322);
+            this.Controls.Add(this.lstDisp);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblOp);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblOp;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnJudge;
+        private System.Windows.Forms.ListBox lstDisp;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "小学测试题";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int a, b,i=1;
+        string op;
+        int result;
+
+        Random rnd = new Random();
+
+     
+
+        private void btnNew_Click_1(object sender, EventArgs e)
+        {
+            this.timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a = rnd.Next(100) + 1;
+            b = rnd.Next(100) + 1;
+            int c = rnd.Next(4);
+            switch (c)
+            {
+                case 0: op = "+"; result = a + b; break;
+                case 1: op = "-"; result = a - b; break;
+                case 2: op = "*"; result = a * b; break;
+                case 3: op = "/"; result = a / b; break;
+            }
+            lblA.Text = a.ToString();
+            lblB.Text = b.ToString();
+            lblOp.Text = op;
+            txtAnswer.Text = "";
+            if(i==1)
+                this.timer2.Start();
+               i++;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            string str = txtAnswer.Text;
+            string disp = "" + a + op + b + "=" + str + " ";
+            string r = "" + result;
+            if (str == r) 
+                disp += "☆";
+            else
+                disp += "╳";
+
+            lstDisp.Items.Add(disp);
+            if(i==11)
+            {
+                this.timer1.Stop();
+                this.timer2.Stop();
+                MessageBox.Show("测试结束");
+
+            }
+        }
+
+        private void btnJudge_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("本次测试采取自动判分，一共十题100以内的算术题，每题间隔5秒");
+        }
+       
+
+
+      
+    }
+}
+
